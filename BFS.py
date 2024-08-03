e66040604b3f8ce0fe443e802d27fc0cc5ede622
Alireza-Kafashha <alireza.kafashha2003@gmail.com>
2024-08-03 16:21:51 +0330

    Adjacencyist & visualization fixed

--- BFS.py
+++ BFS.py
@@ -1,4 +1,6 @@
 import pydotplus
+from IPython.display import Image, display
+import matplotlib.pyplot as plt
 def BFS(start,graph,goal):
 
     visited : []
@@ -24,16 +26,31 @@ def BFS(start,graph,goal):
         if visited[-1] == goal:
             return visited
 
-def AdjacencyList(numberOfEdges):
+def Adjacencyist(numberOfEdges):
     graph = []
+    numberOfEdges = int(numberOfEdges)
     for i in range(numberOfEdges):
-        n = input('Enter the first node :')
-        m = input('Enter the second node :')
+        n = input('Enter the first node : ')
+        m = input('Enter the second node : ')
         graph.append((n,m))
+    return graph
+
+def visualization(graph):
     plt.figure(figsize=(8,8))
     MyGraph = pydotplus.Dot(graph_type='digraph')  
-    for edge in adjacency_list:
+    for edge in graph:
         MyGraph.add_edge(pydotplus.Edge(str(edge[0]), str(edge[1])))
-    img = Image(graph.create_png())
-    display(img)
-        
+    MyGraph.write_png('graph.png')
+    img = plt.imread('graph.png')
+    plt.imshow(img)
+    plt.axis('off')  
+    plt.show()
+
+
+number_of_edges = int(input('Enter number of edges : '))
+starter = input('Enter the root : ')
+target = input('Enter the goal : ')
+
+graph = Adjacencyist(number_of_edges)
+visualization(graph)
+print(BFS(starter,graph,target))
