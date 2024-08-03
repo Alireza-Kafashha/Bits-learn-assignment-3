441e45260f4151f34cc64e52b7fe43b50ff24a2c
Alireza-Kafashha <alireza.kafashha2003@gmail.com>
2024-08-03 15:09:22 +0330

    Create function BFS

--- BFS.py
+++ BFS.py
@@ -1,2 +1,27 @@
-def BFS(start,graph):
-    a
+def BFS(start,graph,goal):
+
+    visited : []
+    explored : [start]
+    frontier: []
+
+    for i in explored:
+
+        for j in range(len(graph)):
+
+            if graph[j][0] == i & graph[j][1] in visited == False :
+
+                frontier.append(graph[j][1])
+
+        explored.append(frontier[0])
+
+        visited.append(explored[0])
+
+        explored = explored[1:]
+
+        frontier = frontier[1:]
+
+        if visited[-1] == goal:
+            return visited
+
+
+
